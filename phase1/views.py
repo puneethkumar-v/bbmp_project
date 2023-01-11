36d82ff475f8a407a7bed868952bd1ad1606e90c
puneethkumar-v <pk.puni872@gmail.com>
2023-01-11 10:55:10 +0530

    Table rendering completed

--- phase1/views.py
+++ phase1/views.py
@@ -3,22 +3,31 @@ import pandas as pd
 
 
 def home(request):
-    f = pd.ExcelFile(
+    # f = pd.ExcelFile(
+    #     '/home/puneethkumarv/Documents/python-project/bbmp_project/phase1/static/package.xlsx')
+    # list_of_dfs = []
+    # for sheet in f.sheet_names:
+
+    #     # Parse data from each worksheet as a Pandas DataFrame
+    #     df = f.parse(sheet)
+    #     print(df)
+
+    # # And append it to the list
+    #     list_of_dfs.append(df)
+
+    # # Combine all DataFrames into one
+    # data = pd.concat(list_of_dfs, ignore_index=True)
+    # # print(data)
+    # # Preview first 10 rows
+    # excel_data = data.head(10).to_json()
+    # # print(data.head(10).to_dict().values())
+    # # excel_data = data
+    df = pd.read_excel(
         '/home/puneethkumarv/Documents/python-project/bbmp_project/phase1/static/package.xlsx')
-    list_of_dfs = []
-    for sheet in f.sheet_names:
+    print(df)
+    out = df.to_numpy().tolist()
+    print(out)
+    out = [tuple(elt) for elt in out]
+    print(out)
 
-        # Parse data from each worksheet as a Pandas DataFrame
-        df = f.parse(sheet)
-
-    # And append it to the list
-        list_of_dfs.append(df)
-
-    # Combine all DataFrames into one
-    data = pd.concat(list_of_dfs, ignore_index=True)
-
-    # Preview first 10 rows
-    excel_data = data.head(10).to_json()
-    print(data.head(10).to_dict().values())
-
-    return render(request, 'base.html', {"excel_data": excel_data})
+    return render(request, 'base.html', {"excel_data": out})
