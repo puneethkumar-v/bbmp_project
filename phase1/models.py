1cd61d1f28331693035b8f0d2eb7b8ad94a37c81
14divya <dsvbhat14@gmail.com>
2023-01-13 18:53:33 +0530

    models updated

--- phase1/models.py
+++ phase1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+class CostSummary(models.Model):
+    
+    items = models.CharField(max_length=500)
+    ars = models.CharField(max_length=100)
+    acrs = models.CharField(max_length=100)
