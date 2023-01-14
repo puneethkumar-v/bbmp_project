134b0bc845a22dade607a9eb00d10d9ce58bdf6d
14divya <dsvbhat14@gmail.com>
2023-01-14 15:21:03 +0530

    files modified

--- phase1/forms.py
+++ phase1/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from bbmpapp.models import CostSummary, Post
-from bbmpapp.resources import RoadResource
+from phase1.models import CostSummary, RoadCostSummary,BOQ, R1_1,Post
+from phase1.resources import RoadResource
 from django.utils.translation import gettext_lazy as _
 
 import re
@@ -9,5 +9,18 @@ class CostSummaryForm(forms.Form):
     class meta:
         model = CostSummary
         fields = '__all__'
+class RoadCostSummaryForm(forms.Form):
+    class meta:
+        model = RoadCostSummary
+        fields = '__all__'
+class BOQForm(forms.Form):
+    class meta:
+        model = BOQ
+        fields = '__all__'
+class R1_1Form(forms.Form):
+    class meta:
+        model = R1_1
+        fields = '__all__'
+
 
 
--- phase1/views.py
+++ phase1/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 #import pandas as pd
-from .models import CostSummary
+from phase1.models import CostSummary,RoadCostSummary,BOQ, R1_1
 from tablib import Dataset
-from .resources import CostSummaryResource
+from phase1.resources import CostSummaryResource,RoadCostSummaryResource,BOQResource
 
 
 #def home(request):
@@ -33,22 +33,63 @@ from .resources import CostSummaryResource
     # print(out)
     # out = [tuple(elt) for elt in out]
     # print(out)
-    def importExcel(request):
-    if request.method == 'POST':
-        cost_summary_resource=CostSummaryResource()
-        dataset = Dataset()
-        new_cost_summary = request.FILES['my_file']
-        imported_data = dataset.load(new_cost_summary.read(),format='xlsx')
-        for data in imported_data:
-            value = CostSummary(
-                data[0],
-                data[1],
-                data[2],
-                data[3]
-                
-            )
-            value.save()
-           
+def importExcel(request):
+	if request.method == 'POST':
+		#cost_summary_resource=CostSummaryResource()
+		dataset1 = Dataset()
+		new_cost_summary = request.FILES["my_file1"]
+		imported_data1 = dataset1.load(new_cost_summary.read(),format='xlsx')
+		for data in imported_data1:
+			value = CostSummary(
+				data[0],
+				data[1],
+				data[2],
+				data[3]
+				
+			)
+			value.save()
+		#road_cost_summary_resource = RoadCostSummaryResource()
+		dataset2 = Dataset()
+		new_road_cost_summary = request.FILES["my_file2"]
+		imported_data2 = dataset2.load(new_road_cost_summary.read(),format='xlsx')
+		for data in imported_data2:
+			value = RoadCostSummary(
+				data[0],
+				data[1],
+				data[2],
+				data[3],
+				data[4]
+				
+				)
+			value.save()
+		#boq_resource=BOQResource()
+		dataset3 = Dataset()
+		new_boq = request.FILES["my_file3"]
+		imported_data3 = dataset3.load(new_boq.read(),format='xlsx')
+		for data in imported_data3:
+			value = BOQ(
+				data[0],
+				data[1],
+				data[2],
+				data[3],
+				data[4],
+				data[5]
+				)
+			
+			value.save()
+		dataset4 = Dataset()
+		new_r1_1 = request.FILES["my_file4"]
+		imported_data4 = dataset4.load(new_r1_1.read(),format='xlsx')
+		for data in imported_data4:
+			value = R1_1(
+				data[0],
+				data[1],
+				data[2],
+				data[3],
+				data[4]
+				)
+			
+			value.save()
+		
 
-
-    return render(request,'form.html')
+	return render(request,'form.html')
